f56a47e1a8319a12fa2d8d6aff08965ffeca1fef
Sunny Chakraborty <sunnyc7@gmail.com>
2018-02-14 00:16:14 -0500

    Updated CAS

--- query/NumberOfUsersToExchServers.ps1
+++ query/NumberOfUsersToExchServers.ps1
@@ -1,9 +1,17 @@
-// Renders a graph of number of users who are connected to Exchange servers daily.
-
+// Renders a graph of EWS connections
 Perf
 | where TimeGenerated > ago(7d)
-// EXCASSERVER1 is like -match
-| where (Computer contains "EXCASSERVER1"  or Computer contains "EXCASSERVER2")
+| where (Computer contains "CAS1"  or Computer contains "CAS2")
 | where CounterName == "Requests/sec"
 | summarize percentiles(CounterValue, 95)  by bin(TimeGenerated, 15m), Computer
+| render timechart
+
+// Renders a graph of number of users who are connected to Exchange servers daily.
+Perf
+| where TimeGenerated > ago(7d)
+// contains is like -match operator from powershell
+// replace CAS with your server name convention.
+| where Computer contains "CAS"
+| where CounterName == "User Count"
+| summarize avg(CounterValue), percentiles(CounterValue, 95)  by bin(TimeGenerated, 15m), Computer
 | render timechart
